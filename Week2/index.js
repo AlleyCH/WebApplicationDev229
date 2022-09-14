8bec203fca36615029307ffe51ab537341e909a5
Alley Chaggar <alleychaggar@gmail.com>
2022-09-14 00:47:38 -0400

    Proper demo formate

--- Week2/index.js
+++ Week2/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Yes, I am awesome!');
+  res.end('Yes, I am awesome!!!!!!!');
 });
 
 server.listen(port, hostname, () => {
